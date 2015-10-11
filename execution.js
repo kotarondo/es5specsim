4f09ca0cf9ca78652eb622892898b541ad763a50
Kotaro Endo <kotarondo@gmail.com>
2015-10-11 21:10:51 +0900

    bug fix: CreateMutableBinding of defined variable in eval

--- execution.js
+++ execution.js
@@ -326,7 +326,7 @@ function DeclarationBindingInstantiation(code, args, func) {
 		var dn = variables[i];
 		var varAlreadyDeclared = env.HasBinding(dn);
 		if (varAlreadyDeclared === false) {
-			env.CreateMutableBinding(dn);
+			env.CreateMutableBinding(dn, configurableBindings);
 			env.SetMutableBinding(dn, undefined, strict);
 		}
 	}
